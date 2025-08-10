2f16e91c4617d2f6dbd0ac13fde11af59038241a
Ilia Fallah <iliafallah138413@gmail.com>
2025-08-10 09:25:31 +0330

    Update OOP Practice.py
    A practice for object-oriented programming

--- OOP Practice.py
+++ OOP Practice.py
@@ -19,6 +19,22 @@ class Car:
         return result
     
 car = Car()
+while True:
+    print("\n\t\t\tMain Menu\n")
+    print("\t1.Show Fares")
+    print("\t2.Rent a Car")
+    print("\t3.Exit\n")
+
+    userChoice = int(input("Please Enter Your Choice: "))
+    if userChoice == 1:
+        car.showCarFare()
+    elif userChoice == 2:
+        carType = input("Which type of car you want to rent? ")
+        dayOfUse = int(input("How many days you want to rent? "))
+        print(f"The cost is ${car.calculateFare(carType, dayOfUse)}")
+    elif userChoice == 3:
+        break
+
 
         
 
